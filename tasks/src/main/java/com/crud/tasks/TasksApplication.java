01db7de016fe3494f743c941c259e4f9fddf5fec
sobek21 <mateuszsobkowicz211@gmail.com>
2020-07-08 21:40:08 +0200

    18.3

---
+++ tasks/src/main/java/com/crud/tasks/TasksApplication.java
@@ -0,0 +1,17 @@
+package com.crud.tasks;
+
+import com.crud.tasks.domain.TaskDto;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.servlet.ServletOutputStream;
+
+@SpringBootApplication
+public class TasksApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TasksApplication.class, args);
+	}
+
+}
