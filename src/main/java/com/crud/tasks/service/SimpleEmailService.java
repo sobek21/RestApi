9230dc88904f2d7ea535c773e7d905484c1425d2
sobek21 <mateuszsobkowicz211@gmail.com>
2020-09-10 18:40:56 +0200

    23.4

---
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -0,0 +1,45 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SimpleEmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(SimpleEmailService.class);
+
+    public void send (Mail mail){
+
+        LOGGER.info("Starting email preparation...");
+        try {
+            SimpleMailMessage mailMessage = createMailMessage(mail);
+            javaMailSender.send(mailMessage);
+
+            LOGGER.info("Email has been sent.");
+        } catch (MailException e) {
+            LOGGER.error("Failed to proces email sending: "+ e.getMessage(), e);
+        }
+    }
+    private SimpleMailMessage createMailMessage(Mail mail){
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(mail.getMailTo());
+            mailMessage.setSubject(mail.getSubject());
+            mailMessage.setText(mail.getMessage());
+            if(mail.getToCC() != null && mail.getToCC().equals("")) {
+                mailMessage.setCc(mail.getToCC());
+            }
+            return mailMessage;
+        }
+
+
+}
+
